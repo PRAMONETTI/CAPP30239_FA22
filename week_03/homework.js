83f2f520346342a60f02902c266173ba926f1a41
Pedro Ramonetti <90287027+PRAMONETTI@users.noreply.github.com>
2022-10-17 21:29:12 -0500

    Homework pushed

--- week_03/homework.js
+++ week_03/homework.js
@@ -14,27 +14,27 @@ d3.csv("library_visits_jan22.csv").then(data => { // We begin by reading the dat
                 .append("svg")
                 .attr("viewbox", [0,0,width,height]);
 
-    const x = d3.scaleBand()                        // Establish the domain of x (the branch) and the range of the margins of the axis
+    const x = d3.scaleBand()      // Establish the constant x (the branch), its domain and the range of the margins of the axis
                 .domain(data.map(d => d.branch)) 
                 .range([margin.left, width - margin.right])
                 .padding(0.1);
 
-    const y = d3.scaleLinear()                                  //Establish the domain of y (the number of visits) and the range of the axis
+    const y = d3.scaleLinear()    //Establish the constant y (the number of visits), its domain and the range of the axis
                 .domain([0, d3.max(data, d => d.num)]).nice()  
                 .range([height - margin.bottom, margin.top]);
     
-    const xAxis = g => g  // Create a g object into which we
+    const xAxis = g => g  // Create the actual axis X and call the constant x
         .attr("transform", `translate ( 0, ${height - margin.bottom +5})`)
         .call(d3.axisBottom(x));
 
-    const yAxis = g => g
+    const yAxis = g => g  // Create the actual axis Y and call the constant y
         .attr("transform", `translate (${margin.left -5}, 0)`)
         .call(d3.axisLeft(y));
 
-    svg.append("g")
+    svg.append("g") // Append the X axis into our svg object
         .call(xAxis);
 
-    svg.append("g")
+    svg.append("g") // Append the Y axis into our svg object
         .call(yAxis);
 
     let bar = svg.selectAll(".bar") // Create the bar objects to append to the chart
