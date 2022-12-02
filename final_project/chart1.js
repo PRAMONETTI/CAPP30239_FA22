3069f856a80d8bb2c56e2cb4f4405a63aa52bc88
Pedro Ramonetti <90287027+PRAMONETTI@users.noreply.github.com>
2022-12-01 18:16:37 -0600

    Transition

--- final_project/chart1.js
+++ final_project/chart1.js
@@ -1,57 +1,53 @@
 const files = ["gender/female.csv","gender/male.csv"]
 
+const chart = document.getElementById('chart1');
+const height = 500;
+const width = 700;
+const margin = ({top: 25, right: 30, bottom: 35, left:50});
+
+const svg = d3.select("#chart1") // Create the svg object as a chart
+    .append("svg")
+    .attr("viewbox", [0,0,width,height]);
+
+const x = d3.scaleBand()    
+    .range([margin.left, width - margin.right])
+    .padding(0.1);
+
+const xAxis = svg.append("g")  // Create the actual axis X and call the constant x
+    .attr("transform", `translate ( 0, ${height - margin.bottom +5})`)
+
+const y = d3.scaleLinear()    //Establish the constant y (the number of visits), its domain and the range of the axis 
+    .range([height - margin.bottom, margin.top]);
+
+const yAxis = svg.append("g")  // Create the actual axis Y and call the constant y
+    .attr("transform", `translate (${margin.left -5}, 0)`)
 
 function UpdateChart(file) {
-    const chart = document.getElementById('chart1');
-    chart.replaceChildren();
+    svg.selectAll(".bar").remove()
     d3.csv(file).then(data => { // We begin by reading the data and transforming our
         // num variable into a number
     for (let d of data) {
     d.Percentage= +d.Percentage;
     }
-    console.log(data);  // Establish the dimensions of the chart
-    const height = 500,
-    width = 700,
-    margin = ({top: 25, right: 30, bottom: 35, left:50});
 
-    let div = d3.select("#chart1") // Create the svg object as a chart
-    .append("svg")
-    .attr("viewbox", [0,0,width,height]);
+    x.domain(data.map(d => d.Possibility));
 
-    const x = d3.scaleBand()      // Establish the constant x (the branch), its domain and the range of the margins of the axis
-    .domain(data.map(d => d.Possibility)) 
-    .range([margin.left, width - margin.right])
-    .padding(0.1);
+    xAxis.call(d3.axisBottom(x));
 
-    const y = d3.scaleLinear()    //Establish the constant y (the number of visits), its domain and the range of the axis
-    .domain([0, d3.max(data, d => d.Percentage)]).nice()  
-    .range([height - margin.bottom, margin.top]);
+    y.domain([0, d3.max(data, d => d.Percentage)]).nice()  
 
-    const xAxis = g => g  // Create the actual axis X and call the constant x
-    .attr("transform", `translate ( 0, ${height - margin.bottom +5})`)
-    .call(d3.axisBottom(x));
+    yAxis.call(d3.axisLeft(y));
 
-    const yAxis = g => g  // Create the actual axis Y and call the constant y
-    .attr("transform", `translate (${margin.left -5}, 0)`)
-    .call(d3.axisLeft(y));
-
-    div.append("g") // Append the X axis into our svg object
-    .call(xAxis);
-
-    div.append("g") // Append the Y axis into our svg object
-    .call(yAxis);
-
-    let bar = div.selectAll(".bar") // Create the bar objects to append to the chart
-    .append("g")
+    let bar = svg.selectAll(".bar") // Create the bar objects to append to the chart
     .data(data)
     .join("g")
     .attr("class","bar");
 
-    bar.append("rect") // Append the bars into the chart, and establish their color and measures
+    bar.append("rect") 
     .attr("fill", "green")
     .attr("x", d => x(d.Possibility))
     .attr("width", x.bandwidth())
-    .attr("y", d => y(d.Percentage))
+    .attr("y", d => y(d.Percentage)).transition().duration(1500)
     .attr("height", d => y(0) - y(d.Percentage));
 
     bar.append("text") // Append the tag of the number of visits above each bar
@@ -68,6 +64,6 @@ UpdateChart(files[0]);
 
 const select = document.getElementById('select');
 select.addEventListener('change', function handleChange(event) {
-    const index = parseInt(event.target.value)
+    const index = parseInt(event.target.value);
     UpdateChart(files[index])
   });
